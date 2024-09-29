9c4ac4dfdf2448ca5745fd69e3048d115817f256
mperor <mpietryga93@gmail.com>
2024-09-29 22:42:06 +0200

    Java Sound API + refactoring JNDI test

--- src/test/java/pl/mperor/lab/java/Java3.java
+++ src/test/java/pl/mperor/lab/java/Java3.java
@@ -7,6 +7,10 @@ import pl.mperor.lab.java.jndi.CustomInitialContextFactory;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Java 1.3 (May 2000)
@@ -16,9 +20,10 @@ public class Java3 {
     @Test
     public void testJavaNamingAndDirectoryInterfaceAkaJNDILookup() throws NamingException {
         System.setProperty(InitialContext.INITIAL_CONTEXT_FACTORY, CustomInitialContextFactory.class.getName());
-        Context context = new InitialContext();
         String bindingName = "hello";
         String bindingResult = "Hello World!";
+
+        Context context = new InitialContext();
         context.bind(bindingName, bindingResult);
         String lookupResult = (String) context.lookup(bindingName);
         Assertions.assertEquals(bindingResult, lookupResult);
@@ -29,4 +34,21 @@ public class Java3 {
         });
     }
 
+    @Test
+    public void testJavaSoundAPI() throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
+        if (AudioSystem.getMixerInfo().length != 0)
+            try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/test/resources/beep.wav"))) {
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                CountDownLatch latch = new CountDownLatch(1);
+                clip.addLineListener(event -> {
+                    if (event.getType() == LineEvent.Type.STOP) {
+                        clip.close();
+                        latch.countDown();
+                    }
+                });
+                clip.start();
+                latch.await();
+            }
+    }
 }
