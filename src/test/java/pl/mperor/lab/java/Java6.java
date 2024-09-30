63f641ba9fd6b4a2a194c620fda29b7772ef1907
mperor <mpietryga93@gmail.com>
2024-09-30 19:17:07 +0200

    Java Compiler API

--- src/test/java/pl/mperor/lab/java/Java6.java
+++ src/test/java/pl/mperor/lab/java/Java6.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Java 1.6 (December 2006) aka Mustang
@@ -18,11 +25,31 @@ public class Java6 {
         ScriptEngine engine = manager.getEngineByName("JavaScript");
 
         // Execute a simple JavaScript expression
-        Assertions.assertEquals(2, engine.eval("const a = 1, b = 1; a + b;"));
+        assertEquals(2, engine.eval("const a = 1, b = 1; a + b;"));
 
         // Call a JavaScript function from Java
         engine.eval("function add(a, b) { return a + b; }");
-        Assertions.assertEquals(2, engine.eval("add(1,1)"));
+        assertEquals(2, engine.eval("add(1,1)"));
+    }
+
+    @Test
+    public void testJavaDynamicCompilation() throws IOException {
+        File sourceFile = new File("./HelloWorld.java");
+        String javaSourceCode = """
+                public class HelloWorld {
+                    public static void main(String[] args) {
+                        System.out.println("Hello World!");
+                    }
+                }
+                """;
+        Files.write(sourceFile.toPath(), javaSourceCode.getBytes());
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        Assertions.assertNotNull(compiler, "JavaCompiler should not be null");
+
+        int compilationResult = compiler.run(null, null, null, sourceFile.getAbsolutePath());
+        Assertions.assertEquals(0, compilationResult, "Compilation should succeed with result 0!");
+        Assertions.assertTrue(sourceFile.delete(), "Source file should be deleted after compilation");
     }
 
 }
