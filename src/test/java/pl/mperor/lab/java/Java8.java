59aff48fa63ab3ee7d2f922e199f759b088e9dd6
mperor <mpietryga93@gmail.com>
2024-10-06 22:10:16 +0200

    Functional Interfaces

--- src/test/java/pl/mperor/lab/java/Java8.java
+++ src/test/java/pl/mperor/lab/java/Java8.java
@@ -5,6 +5,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import pl.mperor.lab.TestUtils;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.function.*;
+
 /**
  * Java 1.8 (March 2014)
  */
@@ -30,6 +36,48 @@ public class Java8 {
         Assertions.assertInstanceOf(Runnable.class, anonymousInnerClass);
         Assertions.assertInstanceOf(Runnable.class, lambdaExpression);
         TestUtils.resetSystemOut();
+    }
+
+    @Test
+    public void testCoreFunctionalInterfaces() {
+        Predicate<?> nullValidator = Objects::isNull;
+        Assertions.assertTrue(nullValidator.test(null));
+
+        var out = TestUtils.setTempSystemOut();
+        Consumer<String> printer = System.out::print;
+        printer.accept("printed");
+        Assertions.assertEquals("printed", out.all());
+        TestUtils.resetSystemOut();
+
+        Supplier<Double> randomGenerator = Math::random;
+        Assertions.assertTrue(randomGenerator.get() < 1.0);
+
+        UnaryOperator<Integer> absoluteUpdater = Math::abs;
+        Assertions.assertEquals(1, absoluteUpdater.apply(-1));
+
+        Function<String, Integer> numberParser = Integer::parseInt;
+        Assertions.assertEquals(1, numberParser.apply("1"));
+    }
+
+    @Test
+    public void testCustomFunctionalInterface() {
+        Testable testable = () -> {
+        };
+        testable.test();
+        // same as Runnable runnable = () -> {};
+
+        Assertions.assertTrue(Testable.class.isAnnotationPresent(FunctionalInterface.class));
+
+        long abstractMethodCount = Arrays.stream(Testable.class.getMethods())
+                .map(Method::getModifiers)
+                .filter(Modifier::isAbstract)
+                .count();
+        Assertions.assertEquals(1, abstractMethodCount);
+    }
+
+    @FunctionalInterface
+    public interface Testable {
+        void test();
     }
 
 }
