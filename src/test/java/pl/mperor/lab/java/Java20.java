d486d36047387b3bc5c718fc105aa3ac7c0daa1f
MP <mpietryga93@gmail.com>
2024-10-23 19:07:12 +0200

    Add Java 20 class with new format ... (#20)
    - samples for standard features
    - class with markdown javadoc with JEPs

---
+++ src/test/java/pl/mperor/lab/java/Java20.java
@@ -0,0 +1,15 @@
+package pl.mperor.lab.java;
+
+/// Java 20™ (March 2023)
+/// [JDK 20](https://openjdk.org/projects/jdk/20)
+///
+/// - PREVIEW & INCUBATOR:
+///     - 429:	Scoped Values (Incubator)
+///     - 432:	Record Patterns (Second Preview)
+///     - 433:	Pattern Matching for switch (Fourth Preview)
+///     - 434:	Foreign Function & Memory API (Second Preview)
+///     - 436:	Virtual Threads (Second Preview)
+///     - 437:	Structured Concurrency (Second Incubator)
+///     - 438:	Vector API (Fifth Incubator)
+public class Java20 {
+}
