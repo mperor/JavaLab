b9f5334d9fb208731cb8d633e61a013601c06f82
mperor <mpietryga93@gmail.com>
2025-01-05 22:14:13 +0100

    Make README files more fancy

--- FirstSteps/src/main/java/pl/mperor/lab/java/Main.java
+++
@@ -1,7 +0,0 @@
-package pl.mperor.lab.java;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Java Basics module.");
-    }
-}
--- JavaReleases/src/main/java/pl/mperor/lab/java/Main.java
+++ JavaReleases/src/main/java/pl/mperor/lab/java/Main.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello JDK features module!");
+        System.out.println("Hello Java key features module!");
         SwingUtilities.invokeLater(SwingForm::new);
     }
 
