ab420788bc0d59c884e37c513c35092792ebb169
MP <mpietryga93@gmail.com>
2025-03-31 22:19:55 +0200

    Add tests for Java built-in schedulers (#70)

--- JavaReleases/src/test/java/pl/mperor/lab/java/Java3.java
+++ JavaReleases/src/test/java/pl/mperor/lab/java/Java3.java
@@ -10,7 +10,11 @@ import javax.naming.NamingException;
 import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Java 1.3 (May 2000)
@@ -36,19 +40,43 @@ public class Java3 {
 
     @Test
     public void testJavaSoundAPI() throws UnsupportedAudioFileException, IOException, LineUnavailableException, InterruptedException {
-        if (AudioSystem.getMixerInfo().length != 0)
-            try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/test/resources/beep.wav"))) {
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioInputStream);
-                CountDownLatch latch = new CountDownLatch(1);
-                clip.addLineListener(event -> {
-                    if (event.getType() == LineEvent.Type.STOP) {
-                        clip.close();
-                        latch.countDown();
-                    }
-                });
-                clip.start();
-                latch.await();
+        if (AudioSystem.getMixerInfo().length == 0) {
+            return;
+        }
+
+        try (AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("src/test/resources/beep.wav"))) {
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            CountDownLatch latch = new CountDownLatch(1);
+            clip.addLineListener(event -> {
+                if (event.getType() == LineEvent.Type.STOP) {
+                    clip.close();
+                    latch.countDown();
+                }
+            });
+            clip.start();
+            latch.await();
+        }
+    }
+
+    @Test
+    public void testScheduledTask() throws InterruptedException {
+        var schedulerCounter = new AtomicInteger(0);
+        var latch = new CountDownLatch(2);
+
+        var scheduler = new TimerTask() {
+            @Override
+            public void run() {
+                schedulerCounter.getAndIncrement();
+                latch.countDown();
             }
+        };
+        var timer = new Timer();
+        timer.scheduleAtFixedRate(scheduler, 0, 100);
+
+        boolean completed = latch.await(300, TimeUnit.MILLISECONDS);
+        Assertions.assertTrue(completed, "Scheduler did not run twice in time");
+        Assertions.assertEquals(2, schedulerCounter.get());
+        timer.cancel();
     }
 }
