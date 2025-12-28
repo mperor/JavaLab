11f3b111e9f0e27e032ddb4772e2f89a119bca82
mperor <mpietryga93@gmail.com>
2025-12-28 20:21:44 +0100

    Add test classes and features for Java 24 and 25
    - includes samples for standard features only
    - adds markdown javadoc for JEPs
    - organizes classes related to Java 24 and 25
    - introduces `CompactSourceFile` and `JavaBaseImport` classes

---
+++ JavaReleases/src/main/java/pl/mperor/lab/java/CompactSourceFile.java
@@ -0,0 +1,8 @@
+/// Is a final top-level class in the unnamed package;
+/// Extends java.lang.Object and does not implement any interfaces;
+/// Has a default constructor with no parameters, and no other constructors;
+/// Has, as its members, the fields and methods in the compact source file; and
+/// Must have a launchable main method; if it does not, a compile-time error is reported.
+void main() {
+    IO.println("Compact source file & main method here ...");
+}
